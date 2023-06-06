7a1a07d22b9d6f4f5158b9b49e39d32b60b9291b
Yue Ni <niyue.com@gmail.com>
2023-06-06 18:01:02 +0800

    Support multiple parameters, and support processing string parameter.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod udf_macro_test;
 extern crate proc_macro;
 
 use quote::{quote, format_ident};
-use syn::ItemFn;
+use syn::{ItemFn, FnArg, PatType, ReturnType};
 
 #[proc_macro]
 pub fn udf(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -15,13 +15,47 @@ pub fn udf_impl(input: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
     let function: ItemFn = syn::parse2(input).unwrap();
     let function_name = &function.sig.ident;
     let return_type = &function.sig.output;
-    let wrapper_name = format_ident!("{}_void", function_name);
+    let mut wrapper_args = Vec::new();
+    let mut call_args = Vec::new();
 
-    let expanded = quote! {
-        #[no_mangle]
-        pub extern "C" fn #wrapper_name() #return_type {
-            #function_name()
+    let mut arg_types = Vec::new();
+    for input in &function.sig.inputs {
+        if let FnArg::Typed(PatType { ty, pat, .. }) = input {
+            let arg_name = pat;
+            if quote!(#ty).to_string() == "& str" {
+                arg_types.push("utf8".to_string());
+                let arg_name_len = format_ident!("{}_len", quote!(#arg_name).to_string());
+                wrapper_args.push(quote! { #arg_name: *const c_char, #arg_name_len: i32 });
+                call_args.push(quote! { str::from_utf8(slice::from_raw_parts(#arg_name as *const u8, #arg_name_len as usize)).unwrap() });
+            } else {
+                arg_types.push(quote!(#ty).to_string());
+                wrapper_args.push(quote! { #arg_name: #ty });
+                call_args.push(quote! { #arg_name });
+            }
+        }
+    }
+
+
+    let wrapper_name = format_ident!("{}_{}", function_name, arg_types.join("_"));
+
+    let expanded = match return_type {
+        ReturnType::Default => {
+            quote! {
+                #[no_mangle]
+                pub extern "C" fn #wrapper_name(#(#wrapper_args),*) {
+                    #function_name(#(#call_args),*)
+                }
+            }
+        }
+        ReturnType::Type(_, ty) => {
+            quote! {
+                #[no_mangle]
+                pub extern "C" fn #wrapper_name(#(#wrapper_args),*) -> #ty {
+                    #function_name(#(#call_args),*)
+                }
+            }
         }
     };
+
     expanded
 }
--- src/udf_macro_test.rs
+++ src/udf_macro_test.rs
@@ -5,15 +5,69 @@ mod tests {
     #[test]
     fn test_no_arg_udf() {
         let input: proc_macro2::TokenStream = quote::quote! {
-            pub fn my_no_arg_udf() -> f64 {
+            pub fn my_udf() -> f64 {
                 1.0
             }
         };
 
         let expected: proc_macro2::TokenStream = quote::quote! {
             #[no_mangle]
-            pub extern "C" fn my_no_arg_udf_void() -> f64 {
-                my_no_arg_udf()
+            pub extern "C" fn my_udf_() -> f64 {
+                my_udf()
+            }
+        };
+        let actual = udf_impl(input);
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn test_i64_arg_udf() {
+        let input: proc_macro2::TokenStream = quote::quote! {
+            pub fn my_udf(x: i64) -> f64 {
+                1.0
+            }
+        };
+
+        let expected: proc_macro2::TokenStream = quote::quote! {
+            #[no_mangle]
+            pub extern "C" fn my_udf_i64(x: i64) -> f64 {
+                my_udf(x)
+            }
+        };
+        let actual = udf_impl(input);
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn test_i64_i32_args_udf() {
+        let input: proc_macro2::TokenStream = quote::quote! {
+            pub fn my_udf(x: i64, y: i32) -> f32 {
+                1.0
+            }
+        };
+
+        let expected: proc_macro2::TokenStream = quote::quote! {
+            #[no_mangle]
+            pub extern "C" fn my_udf_i64_i32(x: i64, y: i32) -> f32 {
+                my_udf(x, y)
+            }
+        };
+        let actual = udf_impl(input);
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn test_str_arg_udf() {
+        let input: proc_macro2::TokenStream = quote::quote! {
+            pub fn my_udf(x: &str) -> bool {
+                true
+            }
+        };
+
+        let expected: proc_macro2::TokenStream = quote::quote! {
+            #[no_mangle]
+            pub extern "C" fn my_udf_utf8(x: *const c_char, x_len: i32) -> bool {
+                my_udf(str::from_utf8(slice::from_raw_parts(x as *const u8, x_len as usize)).unwrap())
             }
         };
         let actual = udf_impl(input);
