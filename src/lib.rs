c1acbc214406aa63c40f1de363288033beee97f1
Yue Ni <niyue.com@gmail.com>
2023-06-07 15:12:54 +0800

    Implement a udf proc macro for generating wrapper code.

--- src/lib.rs
+++ src/lib.rs
@@ -3,35 +3,102 @@ mod udf_macro_test;
 extern crate proc_macro;
 
 use quote::{quote, format_ident};
-use syn::{ItemFn, FnArg, PatType, ReturnType};
+use syn::{parse_macro_input, ItemFn, FnArg, PatType, ReturnType};
 
-#[proc_macro]
-pub fn udf(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_attribute]
+pub fn udf(attrs: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let mut needs_context = false;
+    let _ = _extract_needs_context(attrs, &mut needs_context);
     let input = proc_macro2::TokenStream::from(input);
-    udf_impl(input, false).into()
+    udf_impl(input, needs_context).into()
 }
 
 #[proc_macro]
-pub fn udf_with_context(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = proc_macro2::TokenStream::from(input);
-    udf_impl(input, true).into()
+pub fn context_fns(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    context_fns_impl().into()
+}
+
+fn _extract_needs_context(attrs: proc_macro::TokenStream, needs_context: &mut bool) -> proc_macro::TokenStream {
+    let udf_attr_parser = syn::meta::parser(|meta| {
+        if meta.path.is_ident("needs_context") {
+            *needs_context = true;
+            Ok(())
+        } else {
+            Err(meta.error("unsupported udf macro attribute"))
+        }
+    });
+
+    parse_macro_input!(attrs with udf_attr_parser);
+    proc_macro::TokenStream::new()
 }
 
 fn _extract_params(input: proc_macro2::TokenStream) -> ItemFn {
-    // let mut input = proc_macro2::TokenStream::from(input).into_iter().collect::<Vec<_>>();
-    // let needs_context = match input.pop() {
-    //     Some(proc_macro2::TokenTree::Literal(lit)) => lit.to_string().parse::<bool>().unwrap_or(false),
-    //     token => {
-    //         // if the last token is not a literal, put it back
-    //         input.push(token.unwrap());
-    //         false
-    //     }
-    // };
-    // let input = proc_macro2::TokenStream::from_iter(input.into_iter());
     syn::parse2(input).unwrap()
 }
 
-pub fn udf_impl(input: proc_macro2::TokenStream, needs_context: bool) -> proc_macro2::TokenStream {
+fn context_fns_impl() -> proc_macro2::TokenStream {
+    quote! {
+        #[cfg(not(test))]
+        extern "C" {
+            fn gdv_fn_context_arena_malloc(context: i64, size: i32) -> *mut i8;
+            fn gdv_fn_context_set_error_msg(context: i64, error_msg: *const i8);
+        }
+
+        #[cfg(test)]
+        unsafe fn gdv_fn_context_arena_malloc(_context: i64, size: i32) -> *mut i8 {
+            let mut buffer = Vec::with_capacity(size as usize);
+            let ptr = buffer.as_mut_ptr();
+            std::mem::forget(buffer);
+            ptr as *mut i8
+        }
+
+        #[cfg(test)]
+        unsafe fn gdv_fn_context_set_error_msg(_context: i64, error_msg: *const i8) {
+            let error_msg_str = std::ffi::CStr::from_ptr(error_msg).to_str().unwrap();
+            println!("Error message: {}", error_msg_str);
+        }
+
+        #[cfg(test)]
+        pub(crate) fn gdv_fn_context_arena_free(ptr: *mut i8, size: i32) {
+            unsafe {
+                let buffer = Vec::from_raw_parts(ptr, size as usize, size as usize);
+                drop(buffer);
+            }
+        }
+
+        fn return_gdv_string(ctx: i64, result: &str, out_len: *mut i32) -> *mut libc::c_char {
+            let result_len = result.len() as i32;
+            let result_ptr = unsafe { gdv_fn_context_arena_malloc(ctx, result_len) };
+            if result_ptr.is_null() {
+                unsafe {
+                    gdv_fn_context_set_error_msg(ctx, "Memory allocation failed".as_ptr() as *const libc::c_char);
+                    *out_len = 0;
+                }
+                return std::ptr::null_mut();
+            }
+            unsafe {
+                std::ptr::copy_nonoverlapping(result.as_ptr() as *const u8, result_ptr as *mut u8, result_len as usize);
+                *out_len = result_len;
+            }
+            result_ptr
+        }
+    }
+}
+
+fn _map_type(arg_type: &str) -> String {
+    match arg_type {
+        "i8" => "int8",
+        "i16" => "int16",
+        "i32" => "int32",
+        "i64" => "int64",
+        "f32" => "float32",
+        "f64" => "float64",
+        "& str" => "utf8",
+        _ => arg_type,
+    }.to_string()
+}
+
+fn udf_impl(input: proc_macro2::TokenStream, needs_context: bool) -> proc_macro2::TokenStream {
     let function = _extract_params(input);
     let function_name = &function.sig.ident;
     let return_type = &function.sig.output;
@@ -44,39 +111,58 @@ pub fn udf_impl(input: proc_macro2::TokenStream, needs_context: bool) -> proc_ma
         wrapper_args.push(quote! { ctx: i64 });
     }
 
+
     for input in &function.sig.inputs {
         if let FnArg::Typed(PatType { ty, pat, .. }) = input {
             let arg_name = pat;
-            if quote!(#ty).to_string() == "& str" {
-                arg_types.push("utf8".to_string());
+            let arg_type = quote!(#ty).to_string();
+            // if arg_type is ["i8" | "i16" | "i32" | "i64"] ==> ["int_8" | "int_16" | "int_32" | "int_64"]
+            let arg_type_str = arg_type.as_str();
+            let mapped_gdv_arg_type = _map_type(arg_type_str);
+
+            if mapped_gdv_arg_type == "utf8" {
                 let arg_name_len = format_ident!("{}_len", quote!(#arg_name).to_string());
-                wrapper_args.push(quote! { #arg_name: *const c_char, #arg_name_len: i32 });
-                call_args.push(quote! { str::from_utf8(slice::from_raw_parts(#arg_name as *const u8, #arg_name_len as usize)).unwrap() });
+                wrapper_args.push(quote! { #arg_name: *const libc::c_char, #arg_name_len: i32 });
+                call_args.push(quote! { std::str::from_utf8(
+                    unsafe { std::slice::from_raw_parts(#arg_name as *const u8, #arg_name_len as usize) }
+                ).unwrap() });
             } else {
-                arg_types.push(quote!(#ty).to_string());
                 wrapper_args.push(quote! { #arg_name: #ty });
                 call_args.push(quote! { #arg_name });
             }
+            arg_types.push(mapped_gdv_arg_type);
         }
     }
-
 
     let wrapper_name = format_ident!("{}_{}", function_name, arg_types.join("_"));
 
     let expanded = match return_type {
         ReturnType::Default => {
-            quote! {
-                #[no_mangle]
-                pub extern "C" fn #wrapper_name(#(#wrapper_args),*) {
-                    #function_name(#(#call_args),*)
-                }
-            }
+            panic!("The function to be wrapped must have a return type.");
         }
         ReturnType::Type(_, ty) => {
-            quote! {
-                #[no_mangle]
-                pub extern "C" fn #wrapper_name(#(#wrapper_args),*) -> #ty {
-                    #function_name(#(#call_args),*)
+            // if return type is String, use gandiva context function to allocate memory
+            if quote!(#ty).to_string() == "String" {
+                wrapper_args.push(quote! { out_len: *mut i32 });
+                quote! {
+                    // output the original function
+                    #function
+
+                    #[no_mangle]
+                    pub extern "C" fn #wrapper_name(#(#wrapper_args),*) -> *mut libc::c_char {
+                        let result = #function_name(#(#call_args),*);
+                        return_gdv_string(ctx, &result, out_len)
+                    }
+                }
+            } else {
+                quote! {
+                    // output the original function
+                    #function
+
+                    #[no_mangle]
+                    pub extern "C" fn #wrapper_name(#(#wrapper_args),*) -> #ty {
+                        #function_name(#(#call_args),*)
+                    }
                 }
             }
         }
--- src/udf_macro_test.rs
+++ src/udf_macro_test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::udf_impl;
+    use crate::context_fns_impl;
 
     #[test]
     fn test_no_arg_udf() {
@@ -11,6 +12,8 @@ mod tests {
         };
 
         let expected: proc_macro2::TokenStream = quote::quote! {
+            #input
+
             #[no_mangle]
             pub extern "C" fn my_udf_() -> f64 {
                 my_udf()
@@ -29,8 +32,10 @@ mod tests {
         };
 
         let expected: proc_macro2::TokenStream = quote::quote! {
+            #input
+
             #[no_mangle]
-            pub extern "C" fn my_udf_i64(x: i64) -> f64 {
+            pub extern "C" fn my_udf_int64(x: i64) -> f64 {
                 my_udf(x)
             }
         };
@@ -47,8 +52,10 @@ mod tests {
         };
 
         let expected: proc_macro2::TokenStream = quote::quote! {
+            #input
+
             #[no_mangle]
-            pub extern "C" fn my_udf_i64_i32(x: i64, y: i32) -> f32 {
+            pub extern "C" fn my_udf_int64_int32(x: i64, y: i32) -> f32 {
                 my_udf(x, y)
             }
         };
@@ -65,9 +72,13 @@ mod tests {
         };
 
         let expected: proc_macro2::TokenStream = quote::quote! {
+            #input
+
             #[no_mangle]
-            pub extern "C" fn my_udf_utf8(x: *const c_char, x_len: i32) -> bool {
-                my_udf(str::from_utf8(slice::from_raw_parts(x as *const u8, x_len as usize)).unwrap())
+            pub extern "C" fn my_udf_utf8(x: *const libc::c_char, x_len: i32) -> bool {
+                my_udf(std::str::from_utf8(
+                    unsafe { std::slice::from_raw_parts(x as *const u8, x_len as usize) }
+                ).unwrap())
             }
         };
         let actual = udf_impl(input, false);
@@ -83,12 +94,43 @@ mod tests {
         };
 
         let expected: proc_macro2::TokenStream = quote::quote! {
+            #input
+
             #[no_mangle]
-            pub extern "C" fn my_udf_utf8(ctx: i64, x: *const c_char, x_len: i32) -> bool {
-                my_udf(str::from_utf8(slice::from_raw_parts(x as *const u8, x_len as usize)).unwrap())
+            pub extern "C" fn my_udf_utf8(ctx: i64, x: *const libc::c_char, x_len: i32) -> bool {
+                my_udf(std::str::from_utf8(
+                    unsafe { std::slice::from_raw_parts(x as *const u8, x_len as usize) }
+                ).unwrap())
             }
         };
         let actual = udf_impl(input, true);
         assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn test_needs_context_return_string_udf() {
+        let input: proc_macro2::TokenStream = quote::quote! {
+            pub fn my_udf(x: i64) -> String {
+                String::from("hello")
+            }
+        };
+
+        let expected: proc_macro2::TokenStream = quote::quote! {
+            #input
+
+            #[no_mangle]
+            pub extern "C" fn my_udf_int64(ctx: i64, x: i64, out_len: *mut i32) -> *mut libc::c_char {
+                let result = my_udf(x);
+                return_gdv_string(ctx, &result, out_len)
+            }
+        };
+        let actual = udf_impl(input, true);
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn test_gandiva_context_fns() {
+        let context_fns = context_fns_impl();
+        assert!(!context_fns.is_empty());
     }
 }
